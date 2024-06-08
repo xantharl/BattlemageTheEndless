aacf8483beb81caaf811a899b7ce4258b3575d74
andrew <andrew me>
2024-06-07 19:38:39 -0700

    Made a dummy with a health bar, woohoo

---
+++ Source/BattlemageTheEndless/TrainingDummyActor.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TrainingDummyActor.generated.h"
+
+UCLASS()
+class BATTLEMAGETHEENDLESS_API ATrainingDummyActor : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATrainingDummyActor();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Character)
+	double Health = 100;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
