f35196abec3c7f9a60210a347598504104218b08
andrew <andrew me>
2024-06-15 15:25:43 -0700

    organizing, spell setup, new classes

---
+++ Source/BattlemageTheEndless/MyTP_PickUpComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TP_PickUpComponent.h"
+#include "MyTP_PickUpComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEMAGETHEENDLESS_API UMyTP_PickUpComponent : public UTP_PickUpComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BattlemageTheEndless/PickupActorSpell.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickupActor.h"
+#include "PickupActorSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEMAGETHEENDLESS_API APickupActorSpell : public APickupActor
+{
+	GENERATED_BODY()
+	
+};
